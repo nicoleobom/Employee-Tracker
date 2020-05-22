2a1570cf2e7836e0977bbec2f0738e38e90c494b
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-05-22 17:15:28 -0400

    adding view and add roles

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@ INSERT INTO department (name)
 VALUES ("Sales"), ("Engineering"), ("HR & Finance"), ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Account Manager", 90000, 1), ("Software Engineer", 150000, 2), ("Sales Executive", 135000, 2), ("Finance Manager", 75000, 3), ("Lawyer", 120000, 4);
+VALUES ("Account Manager", 90000, 1), ("Software Engineer", 150000, 2), ("Sales Executive", 135000, 1), ("Finance Manager", 75000, 3), ("Lawyer", 120000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Nicole", "OBomsawin", 1, 1), ("Colin", "Pyzza", 2, 1), ("Stephanie", "Arpin", 3, 2), ("Tyler", "Arpin", 4, 2), ("Freya", "Pyzza", 2, 1);
--- index.js
+++ index.js
@@ -12,104 +12,244 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-	if (err) throw err;
-	console.log('Welcome to the Employee Tracker!');
+    if (err) throw err;
+    console.log('Welcome to the Employee Tracker!');
 
-	start();
+    start();
 });
-
 
 async function start() {
     const questions = await
-    inquirer
-    .prompt([
-        {
-            name: 'userChoices',
-            message: 'What would you like to do?',
-            type: 'list',
-            choices: [
+        inquirer
+            .prompt([
                 {
-                    name: 'View Employees',
-                    value: 'view_employees',
-                },
-                {
-                    name: 'Add Employees',
-                    value: 'add_employees',
-                },
-                {
-                    name: 'Update Employees',
-                    value: 'update_employees',
-                },
-                {
-                    name: 'View Roles',
-                    value: 'view_roles',
-                },
-                {
-                    name: 'Add Roles',
-                    value: 'add_roles',
-                },
-                {
-                    name: 'Update Roles',
-                    value: 'update_roles',
-                },
-                {
-                    name: 'View Departments',
-                    value: 'view_departments',
-                },
-                {
-                    name: 'Add Departments',
-                    value: 'add_departments',
-                },
-                {
-                    name: 'Update Departments',
-                    value: 'update_departments',
-                },
-                {
-                    name: 'Quit',
-                    value: 'quit'
-                }]
-        }
-    ]).then(answers => {
-        switch (answers.userChoices) {
-            case 'view_employees':
-                viewEmployees();
-                break;
-            case 'add_employees':
-                addEmployees();
-                break;
-            case 'update_employees':
-                updateEmployees();
-                break;
-            case 'view_roles':
-                viewRoles();
-                break;
-            case 'add_roles':
-                addRoles();
-                break;
-            case 'update_roles':
-                updateRoles();
-                break;
-            case 'view_departments':
-                viewDepartments();
-                break;
-            case 'add_departments':
-                addDepartments();
-                break;
-            case 'update_departments':
-                updateDepartments();
-                break;
-            case 'quit':
-                quit();
-                break;
-        }
-    });
+                    name: 'userChoices',
+                    message: 'What would you like to do?',
+                    type: 'list',
+                    choices: [
+                        {
+                            name: 'View Employees',
+                            value: 'view_employees',
+                        },
+                        {
+                            name: 'Add Employees',
+                            value: 'add_employees',
+                        },
+                        {
+                            name: 'View Roles',
+                            value: 'view_roles',
+                        },
+                        {
+                            name: 'Add Roles',
+                            value: 'add_roles',
+                        },
+                        {
+                            name: 'Update Roles',
+                            value: 'update_roles',
+                        },
+                        {
+                            name: 'View Departments',
+                            value: 'view_departments',
+                        },
+                        {
+                            name: 'Add Departments',
+                            value: 'add_departments',
+                        },
+                        {
+                            name: 'Update Departments',
+                            value: 'update_departments',
+                        },
+                        {
+                            name: 'Quit',
+                            value: 'quit'
+                        }]
+                }
+            ]).then(answers => {
+                switch (answers.userChoices) {
+                    case 'view_employees':
+                        viewEmployees();
+                        break;
+                    case 'add_employees':
+                        addEmployees();
+                        break;
+                    case 'view_roles':
+                        viewRoles();
+                        break;
+                    case 'add_roles':
+                        addRoles();
+                        break;
+                    case 'update_roles':
+                        updateRoles();
+                        break;
+                    case 'view_departments':
+                        viewDepartments();
+                        break;
+                    case 'add_departments':
+                        addDepartments();
+                        break;
+                    case 'update_departments':
+                        updateDepartments();
+                        break;
+                    case 'quit':
+                        quit();
+                        break;
+                }
+            });
 }
 
 function viewEmployees() {
-    connection.query('SELECT e.first_name, e.last_name, r.title, r.salary, d.name FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id;', 
-      function(err, res) {
+    connection.query('SELECT e.first_name, e.last_name, r.title, r.salary, d.name FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id;',
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            start();
+        })
+};
+
+function addEmployees() {
+    inquirer
+    .prompt([
+        {
+            name: 'fname',
+            type: 'input',
+            message: 'What\'s the employee\'s first name?'
+        },
+        {
+            name: 'lname',
+            type: 'input',
+            message: 'What\'s the employee\'s last name?'
+        },
+        {
+            name: 'role',
+            type: 'list',
+            message: 'What\'s the employee\'s role?',
+            choices: [
+                'account manager',
+                'software engineer',
+                'sales executive',
+                'finance manager',
+                'lawyer'
+            ]
+        },
+        {
+            name: 'manager',
+            type: 'list',
+            message: 'Who is the employee\'s manager?',
+            choices: [
+                'Demi Lovato',
+                'Hilary Duff',
+                'Selena Gomez',
+                'Joe Jonas',
+                'Amanda Bynes'
+            ]
+        }
+    ]).then(answers=> {
+        switch (answers.role) {
+            case "account manager":
+                answers.role = 1;
+                break;
+            case "software engineer":
+                answers.role = 2;
+                break;
+            case "sales executive":
+                answers.role = 3;
+                break;
+            case "finance manager":
+                answers.role = 4;
+                break;
+            case "lawyer":
+                answers.role = 5;
+                break;
+        }
+
+        switch (answers.manager) {
+            case "Demi Lovato":
+                answers.manager = 1;
+                break;
+            case "Hilary Duff":
+                answers.manager = 2;
+                break;
+            case "Selena Gomez":
+                answers.manager = 3;
+                break;
+            case "Joe Jonas":
+                answers.manager = 4;
+                break;
+            case "Amanda Bynes":
+                answers.manager = 5;
+                break;
+        }
+
+        connection.query('INSERT INTO employee SET ?', {
+            first_name: answers.fname,
+            last_name: answers.lname,
+            role_id: answers.role,
+            manager_id: answers.manager,
+            
+        }, function(error) {
+            if (error) throw error;
+            console.log('Employee added!');
+            start();
+        })
+    })
+};
+
+function viewRoles() {
+    connection.query('SELECT * FROM role',
+    function (err, res) {
         if (err) throw err;
         console.table(res);
         start();
-      })
-  };
+    });
+}
+
+function addRoles() {
+    inquirer
+    .prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What\'s the title of the role you\'re adding?'
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'What\'s the salary of the role you\'re adding?'
+        },
+        {
+            name: 'departments',
+            type: 'list',
+            message: 'What\'s the department of the role you\'re adding?',
+            choices: [
+                'Sales',
+                'Engineering',
+                'HR & Finance',
+                'Legal'
+            ]
+        }
+    ]).then(answers=> {
+        switch (answers.departments) {
+            case "Sales":
+                answers.departments = 1;
+                break;
+            case "Engineering":
+                answers.departments = 2;
+                break;
+            case "HR & Finance":
+                answers.departments = 3;
+                break;
+            case "Legal":
+                answers.departments = 4;
+        }
+
+        connection.query('INSERT INTO role SET ?', {
+            title: answers.title,
+            salary: answers.salary,
+            department_id: answers.departments,
+        }, function(error) {
+            if (error) throw error;
+            console.log('Added!');
+            start();
+        })
+    })
+}
